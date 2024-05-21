a5c582a8e346be3c329d2c3cb1a0d9dfa27a257f
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-05-21 14:05:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log("Namaste Dunia29");
+// 
+// var age=21;
+// 
+// if(true)
+// {
+    // console.log(age);
+// }
+
+// function solve(){
+    // var a=29;
+    // console.log(a);
+// }
+// solve();
+
+// let a=25;
+// console.log(a);
+
+// let b=30;
+// console.log(b);
+
+let a=29;
+// a="Bikash";
+// a=true;
---
+++ script.js
@@ -0,0 +1,15 @@
+// let a=20;
+// a=a+10;
+
+// a += 50;
+// console.log(a);
+
+// console.log(5 <= 30);
+// console.log('5' == 5);
+// console.log('s' == 5);
+
+
+// let ans = (true && true && false);
+
+let ans = (false || false || false);
+console.log(ans);
